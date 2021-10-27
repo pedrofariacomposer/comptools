7a187ac88aa294c4ed8f645b533827c748cc89d7
Pedro Proença <pedrofaria1989@gmail.com>
2021-10-27 09:16:01 -0300

    Update __init__.py

--- comptools/__init__.py
+++ comptools/__init__.py
@@ -1,2 +1,1 @@
 __all__ = ['advanced_tools', 'basic_tools', 'graphs', 'hindemith', 'kozu', 'lisparser', 'parsemaker', 'parsepy', 'similarity', 'twelve_tone', '_all_classes']
-from comptools import *
